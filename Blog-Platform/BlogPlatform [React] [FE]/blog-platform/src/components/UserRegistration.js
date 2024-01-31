eaae4189272f01b90f8bb0b36d6f614edd43c241
Vishesh Jaiswal <visheshjaiswal3@gmail.com>
2024-01-31 12:53:33 +0530

    third

--- Blog-Platform/BlogPlatform [React] [FE]/blog-platform/src/components/UserRegistration.js
+++ Blog-Platform/BlogPlatform [React] [FE]/blog-platform/src/components/UserRegistration.js
@@ -10,24 +10,41 @@ function RegisterUser(){
     const [password,setPassword] = useState("");
     const [repassword,setrePassword] = useState("");
     const [role,setRole] = useState("");
-    var setUsernameError=useState("");
+    const [userNameError,setUserNameError]=useState("");
+    const [userEmailError,setUserEmailError]=useState("");
+    const [validEmaid,setValidEmail]=useState(false);
+    const [passwordError,setUserPasswordError]=useState("");
+    const [repasswordError,setUserRePasswordError]=useState("");
+    const emailFormat=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     var checkUserData = ()=>{
+        if(emailFormat.test(userEmail)){
+            setValidEmail
+        }
         if(userEmail==='')
         {
-            setUsernameError("Username cannot be empty");
+            setUserEmailError("User Email cannot be empty");
+            setTimeout(() => setUserEmailError(""), 4000);
             return false;
         }
         if(userName==='')
         {
-            setUsernameError("Username cannot be empty");
+            setUserNameError("User Name cannot be empty");
+            setTimeout(() => setUserNameError(""), 4000);
+            return false;
+        }
+        if(password==='')
+        {
+            setUserPasswordError("Password cannot be empty");
+            setTimeout(() => setUserPasswordError(""), 4000);
+            return false;
+        }
+        if(password==='')
+        {
+            setUserRePasswordError("RePassword cannot be empty");
+            setTimeout(() => setUserRePasswordError(""), 4000);
             return false;
         }
            
-        if(password==='')
-            return false;
-        if(role==='Select Role')
-            return false;
-        return true;
     }
     const signUp = (event)=>{
         event.preventDefault();
@@ -68,21 +85,25 @@ function RegisterUser(){
             <div className="field">
                 <input type="text" required value={userEmail}
                     onChange={(e) => { setUserEmail(e.target.value) }} />
+                    {userEmailError && <label className="alert alert-danger">{userEmailError}</label>}
                 <label>Email Address</label>
             </div>
             <div className="field">
                 <input type="text" required value={userName}
                     onChange={(e) => {setUserName(e.target.value)}}/>
-                    <label>User Name</label>
+                   {userNameError && <label className="alert alert-danger">{userNameError}</label>}
+                <label>User Name</label>
             </div>
             <div className="field">
                 <input type="password" required value={password}
                     onChange={(e) => { setPassword(e.target.value) }} />
+                    {passwordError && <label className="alert alert-danger">{passwordError}</label>}
                 <label>Password</label>
             </div>
             <div className="field">
                     <input type="password" required  value={repassword}
                     onChange={(e) => {setrePassword(e.target.value)}}/>
+                    {repasswordError && <label className="alert alert-danger">{repasswordError}</label>}
                     <label>Re-Type Password</label>
             </div>
             <div className="field">
